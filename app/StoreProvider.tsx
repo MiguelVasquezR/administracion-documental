ab78586a1963ce45ac6b24206ddee5a33569f610
MiguelVasquezR <mvasquesrosas@gmail.com>
2025-01-14 14:15:37 -0600

    subir logo

--- app/StoreProvider.tsx
+++ app/StoreProvider.tsx
@@ -5,10 +5,16 @@ import { Provider } from "react-redux";
 
 import GlobalRenderer from "./GlobalRenderer";
 import store from "./store";
+import { Metadata } from "next";
 
 interface Props {
   readonly children: ReactNode;
 }
+
+export const metadata: Metadata = {
+  title: "Documental Letras Españolas",
+  description: "Sistema de administración de documental de letras españolas",
+};
 
 export const StoreProvider = ({ children }: Props) => {
   return (
